56dbf5f22f5ff87ce2aa233c8d8c5f9f2fbc11c7
ryanbayerCSE <ryanbayer@ucmerced.edu>
2025-05-01 22:05:34 -0700

    Update Canvas.h

--- src/Canvas.h
+++ src/Canvas.h
@@ -5,33 +5,27 @@
 #include <vector>
 #include "Point.h"
 #include "Rectangle.h"
-#include "Triangle.h"
 #include "Circle.h"
-#include "Polygon.h"
+#include "Shape.h"
 
 class Canvas : public bobcat::Canvas_ {
     std::vector<Point*> points;
-    std::vector<Rectangle*> rectangles;
-    std::vector<Triangle*> triangles;
-    std::vector<Circle*> circles;
-    std::vector<Polygon*> polygons;
+    std::vector<Shape*> shapes;
 
 public:
     Canvas(int x, int y, int w, int h);
 
     void addPoint(float x, float y, float r, float g, float b, int size);
 
-    void addRectangle(float x, float y, float width, float height, float r, float g, float b);
-    
-    void addTriangle(float x, float y, float base, float height, float r, float g, float b);
+    void addRectangle(float x, float y, float r, float g, float b);
 
-    void addCircle(float x, float y, float radius, float r, float g, float b);
-
-    void addPolygon(float x, float y, int sides, float length, float r, float g, float b);
+    void addCircle(float x, float y, float r, float g, float b);
 
     void clear();
-    //LALALALALALALALALLAALLALAL
+
     void render();
+
+    Shape* getSelectedShape(float mx, float my);
 };
 
 #endif
